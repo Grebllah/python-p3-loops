b98973eb2fcdf0a0e2d91760a8719f0283e5140f
James Hallberg <Grebllah81788@gmail.com>
2023-11-16 14:08:08 -0500

    loops happy_new_year problem solved

--- lib/looping.py
+++ lib/looping.py
@@ -2,6 +2,13 @@
 
 def happy_new_year():
     # code goes here!
+    count = 10
+    while count >= 0:
+        if count == 0:
+            print("Happy New Year!")
+        else:
+            print(count)
+        count -= 1
     pass
 
 def square_integers(int_list):
